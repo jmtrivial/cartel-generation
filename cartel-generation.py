6ec678e7884ea4f20e4f5a6f34fbd21f36ece172
Jean-Marie Favreau <J-Marie.Favreau@udamail.fr>
2016-11-26 12:14:50 +0100

    Start to implement the SVG generation

--- cartel-generation.py
+++ cartel-generation.py
@@ -3,10 +3,61 @@
 import csv
 import sys
 import getopt
+import array
 
 class Cartel:
 	def __init__(self, row):
+		# TODO: set self._size as "large" or "small"
+		self._size = "large"
+		# TODO set other self._...
 		pass
+	def getSize(self):
+		return self._size
+
+class Format:
+	def __init(self, width, height):
+		self._width = width
+		self._height = height
+	def setNbCartels(self, number):
+		self._number = number
+
+class FinalDocument:
+	
+	def __init__(self, cartelDescriptions):
+		self._format = {}
+		self.addFormat("large", 20, 15)
+		self.addFormat("small", 10, 6)
+		self.initStructure(cartelDescriptions)
+		pass
+	
+	def addFormat(self, formatName, width, height):
+		self._format[formatName] = Cartel(width, height)
+		
+	def initStructure(self, cartelDescriptions):
+		self._cartels = {}
+		for f in self._formats:
+			self._cartels[f.name] = []
+		for desc in cartelDescriptions:
+			cartelSize = desc.getSize()
+			if cartelSize in self._format:
+				self._cartels[cartelSize] = desc
+			else:
+				print "Unknown cartel format"
+			
+	def generate(self, svgfile):
+		computeLayout()
+		# TODO
+		pass
+	def computeLayout(self):
+		# TODO: for each format, get the number of corresponding cartels
+		# compute the number of cartels per line
+		# then compute the number of required lines
+		# then deduce the required height
+		
+		# TODO: then deduce the size of the final document
+		self._documentWidth = 100
+		# self._documentHeight = 
+		
 
 def loadCartelDescriptions(csvfile):
 	result = list()
@@ -50,7 +101,13 @@ def main(argv):
 		print "Error while reading csv file. Abort."
 		sys.exit(4)
 	
-	print 'Output svg file is "'+outputsvgfile+'"'
+	print "Initialize final document"
+	finalDocument = FinalDocument(cartelDescriptions)
+
+	print 'Save final document as "'+outputsvgfile+'"'
+	finalDocument.generate(outputsvgfile)
+	
+	
 
 
 if __name__ == "__main__":
