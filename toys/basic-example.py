381a16314c9bb59009b07132ab5a8d988ace3490
Jean-Marie Favreau <J-Marie.Favreau@udamail.fr>
2016-11-27 10:01:40 +0100

    Switch to a pure XML library

--- toys/basic-example.py
+++ toys/basic-example.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import svgwrite
+import xml.etree.cElementTree as ET
 
 avenirStyle = "font-style:normal;font-variant:normal;font-weight:500;font-stretch:normal;line-height:125%;font-family:Avenir;-inkscape-font-specification:'Avenir, Medium';text-align:start;writing-mode:lr-tb;text-anchor:start;"
 
-dwg = svgwrite.Drawing(filename = "example.svg", size = ("20cm", "15cm"))
+root = ET.Element("svg", {"version":"1.1", "xmlns:xlink":"http://www.w3.org/1999/xlink", "xmlns":"http://www.w3.org/2000/svg", "height":"15cm", "width":"20cm"})
 
-dwg.add(dwg.image("../img/vermont_bg_big.png", insert = ("0cm", "0cm"), size = ("20cm", "15cm")))
+image = ET.SubElement(root, "image", {"height":"15cm", "width":"20cm", "x":"0", "y":"0", "xlink:href":"../img/vermont_bg_big.png"})
 
-dwg.add(dwg.text("Roget Jourdain  (1845-1918)", insert = ("2cm", "2cm"), style = avenirStyle + "font-size:21.84086609px"))
+author = ET.SubElement(root, "text", {"style":avenirStyle + "font-size:21.84086609px", "x":"2cm", "y":"2cm"}).text = "Roget Jourdain  (1845-1918)"
+
+
+#dwg = svgwrite.Drawing(filename = "example.svg", size = ("20cm", "15cm"))
+
+#dwg.add(dwg.image("../img/vermont_bg_big.png", insert = ("0cm", "0cm"), size = ("20cm", "15cm")))
+
+#dwg.add(dwg.text("Roget Jourdain  (1845-1918)", insert = ("2cm", "2cm"), style = avenirStyle + "font-size:21.84086609px"))
 
 #title = dwg.tspan("Élection du Conseil municipal, tableau récapitulatif des votes", style = avenirStyle + "font-size:47.67940903px")
 #date = dwg.tspan("19ème siècle", style = avenirStyle + "font-size:23.15856925px")
@@ -17,4 +24,7 @@ dwg.add(dwg.text("Roget Jourdain  (1845-1918)", insert = ("2cm", "2cm"), style =
 #titleDateArea.add(title)
 #titleDateArea.add(date)
 #dwg.add(titleDateArea)
-dwg.save()
+#dwg.save()
+
+tree = ET.ElementTree(root)
+tree.write("example.svg")
