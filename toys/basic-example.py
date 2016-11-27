83d9b0ed664445f728a37cd8c1809ce160e0cacf
Jean-Marie Favreau <J-Marie.Favreau@udamail.fr>
2016-11-27 09:50:38 +0100

    Try to introduce texts in boxes with svgwrite (and fail)

--- toys/basic-example.py
+++ toys/basic-example.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import svgwrite
 
+avenirStyle = "font-style:normal;font-variant:normal;font-weight:500;font-stretch:normal;line-height:125%;font-family:Avenir;-inkscape-font-specification:'Avenir, Medium';text-align:start;writing-mode:lr-tb;text-anchor:start;"
 
 dwg = svgwrite.Drawing(filename = "example.svg", size = ("20cm", "15cm"))
 
 dwg.add(dwg.image("../img/vermont_bg_big.png", insert = ("0cm", "0cm"), size = ("20cm", "15cm")))
-name = dwg.text("Roget Jourdain  (1845-1918)", insert = ("66", "77"), style = "font-style:normal;font-weight:normal;font-size:21.84115791px;line-height:125%;font-family:sans-serif;text-align:start;letter-spacing:0px;word-spacing:0px;writing-mode:lr-tb;text-anchor:start;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1")
-dwg.add(name)
 
+dwg.add(dwg.text("Roget Jourdain  (1845-1918)", insert = ("2cm", "2cm"), style = avenirStyle + "font-size:21.84086609px"))
 
+#title = dwg.tspan("Élection du Conseil municipal, tableau récapitulatif des votes", style = avenirStyle + "font-size:47.67940903px")
+#date = dwg.tspan("19ème siècle", style = avenirStyle + "font-size:23.15856925px")
+#titleDateArea = dwg.textArea(insert = ("70", "113"), size= ("668", "166"))
+#titleDateArea.add(title)
+#titleDateArea.add(date)
+#dwg.add(titleDateArea)
 dwg.save()
