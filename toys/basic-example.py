28db9b30bbddf0270d17131002ba6bc03810fed7
Jean-Marie Favreau <J-Marie.Favreau@udamail.fr>
2016-11-27 01:17:06 +0100

    Add a text

--- toys/basic-example.py
+++ toys/basic-example.py
@@ -5,6 +5,9 @@ import svgwrite
 
 dwg = svgwrite.Drawing(filename = "example.svg", size = ("20cm", "15cm"))
 
-dwg.add(dwg.image("../img/vermont_bg_big.png", insert = ("0px", "0px"), size = ("20cm", "15cm")))
-		
+dwg.add(dwg.image("../img/vermont_bg_big.png", insert = ("0cm", "0cm"), size = ("20cm", "15cm")))
+name = dwg.text("Roget Jourdain  (1845-1918)", insert = ("66", "77"), style = "font-style:normal;font-weight:normal;font-size:21.84115791px;line-height:125%;font-family:sans-serif;text-align:start;letter-spacing:0px;word-spacing:0px;writing-mode:lr-tb;text-anchor:start;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1")
+dwg.add(name)
+
+
 dwg.save()
